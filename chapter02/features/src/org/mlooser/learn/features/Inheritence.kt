cf2abbf2d0ddc1c53290ae00abe13dce9e368912
mlooser <mlooser@gmail.com>
2020-03-06 14:03:58 +0100

    [features] interfaces - more properties

--- chapter02/features/src/org/mlooser/learn/features/Inheritence.kt
+++ chapter02/features/src/org/mlooser/learn/features/Inheritence.kt
@@ -9,7 +9,8 @@ enum class CardType {
 open class Payment(val amount: BigDecimal)
 
 class CardPayment(amount: BigDecimal, val number: String, val type: CardType) : Payment(amount) {
-
+    val description
+        get() = "$amount $number $type"
 }
 
 class ChequePayment : Payment {
@@ -18,15 +19,16 @@ class ChequePayment : Payment {
         this.bankId = bankId;
     }
 
-    var name: String
-        get() = this.name
-
-    var bankId: String
-        get() = this.bankId
+    private var name: String
+    private var bankId: String
+    val description
+        get() = "$amount $name $bankId"
 }
 
-fun main(){
+fun main() {
     val cardPayment = CardPayment(BigDecimal.TEN, "123", CardType.Visa)
     val chequePayment = ChequePayment(BigDecimal.TEN, "Ma Lu", "pko")
-    println("OK")
+
+    println("Card Payment ${cardPayment.description}")
+    println("Cheque Payment ${chequePayment.description}")
 }
