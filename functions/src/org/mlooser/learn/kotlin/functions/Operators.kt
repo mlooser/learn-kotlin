66946a3d44a4454fabd8609815711993a321074a
mlooser <mlooser@gmail.com>
2020-03-20 12:26:57 +0100

    [functions] plus/minusAssign

--- functions/src/org/mlooser/learn/kotlin/functions/Operators.kt
+++ functions/src/org/mlooser/learn/kotlin/functions/Operators.kt
@@ -5,8 +5,18 @@ class Vector2D(var x: Float, var y: Float) {
         return Vector2D(x + other.x, y + other.y)
     }
 
+    operator fun plusAssign(other: Vector2D) {
+        x += other.x
+        y += other.y
+    }
+
     operator fun minus(other: Vector2D): Vector2D {
         return Vector2D(x - other.x, y - other.y)
+    }
+
+    operator fun minusAssign(other: Vector2D) {
+        x -= other.x
+        y -= other.y
     }
 
     operator fun unaryMinus(): Vector2D {
@@ -34,14 +44,17 @@ class Cirlce(var middle: Vector2D, var radius: Float) {
 
 }
 
-fun main(){
-    val p1 = Vector2D(2f,2f)
+fun main() {
+    val p1 = Vector2D(2f, 2f)
     val p2 = Vector2D(1f, 1f)
 
     println("$p1 - $p2 = ${p1 - p2}")
 
-    val circle = Cirlce(Vector2D(0f,0f),3f)
-    val p3 = Vector2D(1f,1f)
+    p1 += p2;
+    println("p1 += p2 = $p1")
+
+    val circle = Cirlce(Vector2D(0f, 0f), 3f)
+    val p3 = Vector2D(1f, 1f)
 
     println("$circle contains $p3 = ${p3 in circle}")
 }
