52ae221aa4139c43f0ab548a50ec5d1d115d834f
marcin_lusa <mlooser@gmail.com>
2020-03-26 22:03:51 +0100

    [functional] assigning function - compilation fix

--- functional/src/main/kotlin/org/mlooser/learn/kotlin/functional/Anonymus.kt
+++ functional/src/main/kotlin/org/mlooser/learn/kotlin/functional/Anonymus.kt
@@ -2,7 +2,7 @@ package org.mlooser.learn.kotlin.functional
 
 fun main() {
     val ints = (1..10).toList()
-    val evens = ints.filter(fun(i)git  = i % 2 == 0)
+    val evens = ints.filter(fun(i) = i % 2 == 0)
 
     println("Evens of $ints = $evens")
 }
